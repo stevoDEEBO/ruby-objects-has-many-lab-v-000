a32dcd0a9c05245f386b0a14d688db974f3ee669
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-28 08:05:01 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post
+  attr_accessor :author, :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if self.author == nil
+      nil
+    else
+      self.author.name
+    end
+  end
+
+end
