0c42964fb67afa6e778197b397659d9745dc7434
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-28 08:02:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,27 @@
 class Artist
   attr_accessor :name
+  @@songs = []
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   def add_song(song)
-    @songs << song
+    @@songs << song
     song.artist = self
   end
 
   def songs
-    @songs
+    @@songs
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
+    @@songs << song
     song.artist = self
   end
 
   def self.song_count
-    @songs.length
+    @@songs.length
   end
 end
