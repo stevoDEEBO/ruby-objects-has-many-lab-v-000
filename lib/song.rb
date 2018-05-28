616672d693de3d42ed24a450a46fcea86d755e9d
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-28 07:57:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,10 @@ class Song
   end
 
   def artist_name
-    if self.artist == nil ? nil : self.artist.name
+    if self.artist == nil
+      nil
+    else
+      self.artist.name
     end
   end
 
