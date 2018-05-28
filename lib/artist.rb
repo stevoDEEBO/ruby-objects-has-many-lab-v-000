4a26d32d6fe3bbe0289d575fc004a1a27ba31be9
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-28 07:07:19 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+ 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+ 
+  def songs
+    @songs
+  end
+
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    @songs << song
+    song.artist = self
+  end
+end
