cf37cc276808da843f52fe087723b726b8ea8393
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-28 08:06:53 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,9 @@
 class Author
-  attr_accessor :title
+  attr_accessor :name
   @@posts = []
 
-  def initialize(title)
-    @title = title
+  def initialize(name)
+    @name = name
   end
 
   def add_post(post)
